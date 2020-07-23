37bdf30aeaf3244a3ae2a9966cce730a0e8ac36a
sepehrmn <sepehr.mahmoudian@gmail.com>
2020-07-23 21:46:36 +0200

    plotting updates

--- code/plotting.py
+++ code/plotting.py
@@ -3,17 +3,18 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def _plot_subplot(label, metric, number, function, results):
+def _plot_subplot(label, metric, total_number, number, function, results):
     """Plot a subplot.
 
     :param label: string - label of the subplot
     :param metric: string - information metric
+    :param total_number: int - total number of columns (subplots) for the plot
     :param number: int - number of the subplot
     :param function: string - activation function
     :param results: structured array - results of the analysis
     :return: None
     """
-    ax = plt.subplot(3, 1, number, projection='3d')
+    ax = plt.subplot(1, total_number, number, projection='3d')
     ax.set_xlabel('r', labelpad=15, fontsize=22)
     ax.set_ylabel('c', labelpad=15, fontsize=22)
     ax.xaxis.set_tick_params(labelsize=11)
@@ -40,14 +41,14 @@ def plot_surfaceplots(results):
     :param results: structured array - results of the analysis
     :return: None
     """
-    plt.figure(figsize=(9, 10.8))
+    plt.figure(figsize=(25, 11))
 
-    _plot_subplot("(a)", 'I_X_C__R', 1, 'additive', results)
-    _plot_subplot("(b)", 'I_X_C__R', 2, 'modulatory', results)
-    _plot_subplot("(c)", 'I_X_C__R', 3, 'both', results)
+    _plot_subplot("(a)", 'I_X_C__R', 3, 1, 'additive', results)
+    _plot_subplot("(b)", 'I_X_C__R', 3, 2, 'modulatory', results)
+    _plot_subplot("(c)", 'I_X_C__R', 3, 3, 'both', results)
 
     plt.subplots_adjust(wspace=None, hspace=0.2)
-    plt.show()
+    plt.savefig("classical_terms.png")
 
     return
 
